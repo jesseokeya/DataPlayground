70ef3d0b1dc50c99c85bf631d960540c4ab3bf90
Jesse Okeya <Jesseokeya@gmail.com>
2017-10-15 22:42:00 -0400

    Bug Fix ⚙️

---
+++ src/execute.py
@@ -0,0 +1,32 @@
+'''
+    Name: Data Playground
+    Aurthor: Jesse Okeya && Larry Agbana
+    Version: 0.0.1
+    Description: Sorting, Analyzing and Vizualizing Bank Data Collected
+'''
+
+# import dependencies
+import index
+import csv as csv
+import numpy as np
+import json
+
+# execute main function
+def main():
+   # import csv file to be analized(data Source)
+   data = csv.reader(open('../resources/bankData.csv'))
+   # range of data (index at 0 is minimum, index at 1 is maximum)
+   data_range = [1, 10000]
+
+   index.main()
+
+   # sort data into dictionaries with access keys
+   # the access keys are the criterias
+   sorted_data = index.sort_data(data)
+   find_person = index.search_by_surname('Obinna', sorted_data)
+
+   # print a viewable clean version of the dictionary in json format
+   print(json.dumps(find_person, indent=1))
+
+# excecute the main function
+main()
--- src/index.py
+++ src/index.py
@@ -1,62 +1,22 @@
-'''
-    Name: Data Playground
-    Aurthor: Jesse Okeya && Larry Agbana
-    Version: 0.0.1
-    Description: Sorting, Analyzing and Vizualizing Bank Data Collected
-'''
-
-# import dependencies
-import csv as csv
-import numpy as np
-import json
-
-# execute main function
-def main():
-   # import csv file to be analized(data Source)
-   data = csv.reader(open('../resources/bankData.csv'))
-   # range of data (index at 0 is minimum, index at 1 is maximum)
-   data_range = [1, 10000]
-   # list of all search criterias that could be used
-   searchCriterias = [
-       'CustomerId',
-       'Surname',
-       'CreditScore',
-       'Geography',
-       'Gender',
-       'Age',
-       'Tenure',
-       'Balance',
-       'NumOfProducts',
-       'HasCrCard',
-       'IsActiveMember',
-       'EstimatedSalary',
-       'Exited'
-       ]
-   # sort data into dictionaries with access keys
-   # the access keys are the criterias
-   sorted_data = sort_data(data)
-   find_person = search_by_surname('Obinna', sorted_data, searchCriterias)
-
-   # print a viewable clean version of the dictionary in json format
-   print(json.dumps(find_person, indent=1))
 
 # function to sort all of the data by the users surname given
 # returns a dictionary of all data related to that person
-def search_by_surname(name, data, searchCriterias):
+def search_by_surname(name, data):
     result = None
     index = -1
 
     for i in range(len(data['Surname'])):
         if(data['Surname'][i].lower() == name.lower()):
             index = i + 1
-            result = search_by_index(index, data, searchCriterias)
+            result = search_by_index(index, data)
             return result
 
     return result;
 
 # function find each person by index
 # returns person in that index and all data relating to that person
-def search_by_index(index, data, searchCriterias):
+def search_by_index(index, data):
+    searchCriterias = return_all_criterias()
     index = index - 1
     result = {}
 
@@ -64,6 +24,15 @@ def search_by_index(index, data, searchCriterias):
         result[searchCriterias[i]] = data[searchCriterias[i]][index]
 
     return result
+
+# list of all search criterias that could be used
+def return_all_criterias():
+    searchCriterias = [
+        'CustomerId', 'Surname', 'CreditScore', 'Geography',
+        'Gender', 'Age', 'Tenure', 'Balance', 'NumOfProducts',
+        'HasCrCard', 'IsActiveMember', 'EstimatedSalary', 'Exited'
+        ]
+    return searchCriterias
 
 # function to sort all data into a dictionary using search criterias as key
 # returns a dictionary of all search criterias with all specific data
@@ -87,6 +56,7 @@ def sort_data(data_):
 
     return trackDataKeys
 
-
-# excecute the main function
-main()
+def main():
+    print('\n')
+    print('Welcome To Data Playground')
+    print('\n')
