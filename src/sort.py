c55eb20b8094633f10435060df33755e36eb451d
Jesse Okeya <Jesseokeya@gmail.com>
2017-12-09 10:59:39 -0500

    stable version 1.0.0 alpha ✓

--- src/sort.py
+++ src/sort.py
@@ -1,3 +1,10 @@
+'''
+    Name: Data Playground
+    Aurthor: Jesse Okeya && Larry Agbana
+    Version: 0.0.1
+    Description: Sorting, Analyzing and Vizualizing Bank Data Collected
+'''
+
 # import dependencies
 import os
 import csv
---
+++ src/tests.py
@@ -0,0 +1,12 @@
+'''
+    Name: Data Playground
+    Aurthor: Jesse Okeya && Larry Agbana
+    Version: 0.0.1
+    Description: Sorting, Analyzing and Vizualizing Bank Data Collected
+'''
+
+def main():
+    print('Running Some Tests....')
+
+
+main()
